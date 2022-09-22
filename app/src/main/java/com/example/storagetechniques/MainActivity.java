0b832f9196d3ac9bfd0b4613fc766e42cbc51137
DELL <s.hossam@nu.edu.eg>
2022-09-22 09:10:25 +0200

    SharedPreferences

--- app/src/main/java/com/example/storagetechniques/MainActivity.java
+++ app/src/main/java/com/example/storagetechniques/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.storagetechniques;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     EditText passEdit, userEdit;
     String adminPass ="123";
     String adminUser ="SaraH";
+    CheckBox rememberMeCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,28 +28,46 @@ public class MainActivity extends AppCompatActivity {
         LoginBtn = findViewById(R.id.login_btn);
         passEdit = findViewById(R.id.password_edit);
         userEdit = findViewById(R.id.username_edit);
+        rememberMeCheck = findViewById(R.id.rememberme_check);
+
 
         LoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Password and Username Validity
+                // Password and Username Validity Check
 
                 String user = userEdit.getText().toString();
                 String pass = passEdit.getText().toString();
-                if (user == ""){
+                if (user.isEmpty()){
                     Toast.makeText(MainActivity.this, "Username cannot be empty", Toast.LENGTH_SHORT).show();
                 }
-                else if (pass == ""){
+                else if (pass.isEmpty()){
                     Toast.makeText(MainActivity.this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
                 }
-                else if (user != adminUser){
+                else if (!user.equals(adminUser)){
                     Toast.makeText(MainActivity.this, "Username is incorrect", Toast.LENGTH_SHORT).show();
                 }
-                else if (pass != adminPass){
+                else if (!pass.equals(adminPass)){
                     Toast.makeText(MainActivity.this, "Password is incorrect", Toast.LENGTH_SHORT).show();
                 }
                 else {
                     Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+
+                    // If the user wants to save username and password (remember me)
+                    if (rememberMeCheck.isChecked()){
+
+                        // Get the app context (the place we want to get the preferences to), then get the preferences
+                        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("AuthPref", Context.MODE_PRIVATE);
+                        // Get an instance of sharedPref editor (SharedPreferences.Editor editor) and use it to edit the sharedPref instance we have (sharedPreferences)
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                        // Store Shared Preferences
+                        editor.putString(SplashActivity.USER_KEY,user);
+                        editor.putString(SplashActivity.PASS_KEY,pass);
+                        editor.apply();
+
+                    }
+
                     Intent i= new Intent(MainActivity.this, MainActivity2.class);
                     startActivity(i);
                 }
--- app/src/main/java/com/example/storagetechniques/SplashActivity.java
+++ app/src/main/java/com/example/storagetechniques/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.storagetechniques;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -10,6 +12,8 @@ import android.util.Log;
 public class SplashActivity extends AppCompatActivity {
 
     Handler handler;
+    public static final String USER_KEY = "user";
+    public static final String PASS_KEY = "pass";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +23,23 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i= new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(i);
+                // Get Shared Preferences while splash is loading so as to decide where to intent
+
+                // Get the app context (the place we want to get the preferences to), then get the preferences
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("AuthPref", Context.MODE_PRIVATE);
+
+                // Read if we have sharedPref
+                String storedUser = sharedPreferences.getString(SplashActivity.USER_KEY, null);
+                String storedPass = sharedPreferences.getString(SplashActivity.PASS_KEY, null);
+
+                if (storedPass.isEmpty() || storedUser.isEmpty()){
+                    Intent i= new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Intent i2= new Intent(SplashActivity.this, MainActivity2.class);
+                    startActivity(i2);
+                }
             }
         }, 5000);
     }
